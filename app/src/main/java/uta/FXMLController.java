a83b1181d6b1ac17c709ae8f31549599206e9e19
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2024-11-18 01:05:33 +0800

    feat: Enhance UI with title text and SVG graphics in scene layout

--- app/src/main/java/uta/FXMLController.java
+++ app/src/main/java/uta/FXMLController.java
@@ -2,13 +2,19 @@ package uta;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
 
 public class FXMLController {
+
+    @FXML
+    private Text title;
 
     @FXML
     private Label label;
 
     public void initialize() {
-        label.setText("Uta Music Player");
+        Font poppinsBold = Font.loadFont(getClass().getResourceAsStream("/uta/Poppins-Bold.ttf"), 30);
+        title.setFont(poppinsBold);
     }
 }
