7241e457b154326bf9580800da1818427a9f6d23
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2024-11-17 18:32:03 +0800

    feat: Update application title and label

--- app/src/main/java/uta/FXMLController.java
+++ app/src/main/java/uta/FXMLController.java
@@ -9,8 +9,6 @@ public class FXMLController {
     private Label label;
 
     public void initialize() {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        label.setText("Uta Music Player");
     }
 }
